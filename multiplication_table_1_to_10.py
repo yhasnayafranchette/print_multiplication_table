4c2aec9fdbd16b6eb47f7d4ee04bad92757ad56b
yhasnayafranchette <yhasnayafranchettesoriano@gmail.com>
2024-01-23 00:41:47 +0800

    print multiplication table

--- multiplication_table_1_to_10.py
+++ multiplication_table_1_to_10.py
@@ -1,3 +1,8 @@
 #Exercise 13: Print multiplication table from 1 to 10
 
 #Print multiplication table from 1 to 10 in matrix format
+for multiplicand in range(1, 11):
+    for multiplier in range(1, 11):
+        result = multiplicand * multiplier
+        print(f"{result:4}", end=" ") 
+    print()  
